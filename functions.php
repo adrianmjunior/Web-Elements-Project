16840279f181a0f98ba80df4d386d3f79697e90d
Adrian <owner@AdriansMac.local>
2018-04-02 21:41:41 -0500

    first upload

---
+++ functions.php
@@ -0,0 +1,18 @@
+<?php
+function my_theme_enqueue_styles() {
+
+    $parent_style = 'twentyseventeen'; // This is 'twentyfifteen-style' for the Twenty Fifteen theme.
+
+
+    // brings the parent theme styles
+    wp_enqueue_style( 'twentyseventeen', get_template_directory_uri() . '/style.css' );
+
+    // brings the child theme styles
+    wp_enqueue_style( 'child-style',
+        get_stylesheet_directory_uri() . '/style.css',
+        array( $parent_style ),
+        wp_get_theme()->get('Version')
+    );
+}
+add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
+?>
